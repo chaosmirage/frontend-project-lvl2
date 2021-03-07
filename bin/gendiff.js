786d32c6faa143646e7a4dbd7013651476285ffd
Adel Khamatov <adelkhamatov@yandex.ru>
2021-03-07 20:43:38 +0300

    Feature: add arguments and option to cli

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,5 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander/esm.mjs';
+import cli from '../src/cli.js';
 
-const program = new Command();
-
-program.description('Compares two configuration files and shows a difference').version('0.0.1');
-
-program.parse();
+cli();
