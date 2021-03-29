3790997d6f4f0d16a13a3d34b461424c0e3c56f1
Adel Khamatov <adelkhamatov@yandex.ru>
2021-03-29 20:39:08 +0300

    Feature: log arguments and options

--- src/cli.js
+++ src/cli.js
@@ -7,7 +7,12 @@ export default () => {
     .description('Compares two configuration files and shows a difference')
     .version('0.0.1')
     .arguments('<filepath1> <filepath2>')
-    .option('-f, --format [type]', 'output format');
+    .option('-f, --format [type]', 'output format')
+    .action((filepath1, filepath2, options) => {
+      console.log(filepath1);
+      console.log(filepath2);
+      console.log(options);
+    });
 
   program.parse();
 };
