af08ddcead1e1a0104f5d89c7d991ed94cef3c9b
Adel Khamatov <adelkhamatov@yandex.ru>
2021-04-08 10:02:28 +0300

    Feature: implement comparsion of nested structures

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,19 @@
+import { test, expect } from '@jest/globals';
+import gendiff from '../src/index.js';
+import readFile from '../src/utils/readFixtureFile.js';
+
+const result = readFile('result.txt').trim();
+
+test('Сравнение JSON файлов', () => {
+  const filepath1 = '/json/file1.json';
+  const filepath2 = '/json/file2.json';
+
+  expect(gendiff({ filepath1, filepath2, readFile, format: 'json' })).toBe(result);
+});
+
+test('Сравнение YAML файлов', () => {
+  const filepath1 = '/yaml/file1.yml';
+  const filepath2 = '/yaml/file2.yml';
+
+  expect(gendiff({ filepath1, filepath2, readFile, format: 'yaml' })).toBe(result);
+});
--- src/index.js
+++ src/index.js
@@ -19,71 +19,241 @@ const STATES = {
   modified: 'modified',
 };
 
-const makeDiff = (file1Content, file2Content) => {
-  const uniqJoinedKeys = Array.from(new Set([...Object.keys(file1Content), ...Object.keys(file2Content)]));
+const makeDiff = (content1, content2) => {
+  const iter = (currentLevelContent1 = null, currentLevelContent2 = null) => {
+    const uniqJoinedKeys = Array.from(
+      new Set([...Object.keys(currentLevelContent1 || {}), ...Object.keys(currentLevelContent2 || {})])
+    );
 
-  return uniqJoinedKeys.reduce((acc, key) => {
-    const file1ContentValue = file1Content[key];
-    const file2ContentValue = file2Content[key];
+    return uniqJoinedKeys.reduce((acc, key) => {
+      const file1ContentValue = _.get(currentLevelContent1, key);
+      const file2ContentValue = _.get(currentLevelContent2, key);
 
-    const isNotChanged = file1ContentValue === file2ContentValue;
+      const isValuesPlainObject = _.isPlainObject(file1ContentValue) && _.isPlainObject(file2ContentValue);
 
-    if (isNotChanged) {
-      return [...acc, { key, state: STATES.initial, value: file1ContentValue }];
-    }
+      const isObjectsValueDoesNotModified =
+        isValuesPlainObject && _.isEqual(file1ContentValue, file2ContentValue);
+      if (isObjectsValueDoesNotModified) {
+        return {
+          ...acc,
+          [key]: {
+            state: STATES.initial,
+            value: iter(file1ContentValue, file2ContentValue),
+          },
+        };
+      }
 
-    const isDeleted = _.has(file1Content, key) && !_.has(file2Content, key);
-    if (isDeleted) {
-      return [...acc, { key, state: STATES.deleted, value: file1ContentValue }];
-    }
+      const isObjectValueModifiedToAnotherObjectValue =
+        isValuesPlainObject && !_.isEqual(file1ContentValue, file2ContentValue);
+      if (isObjectValueModifiedToAnotherObjectValue) {
+        return {
+          ...acc,
+          [key]: {
+            state: STATES.modified,
+            value: iter(file1ContentValue, file2ContentValue),
+            prevValue: iter(file1ContentValue, file1ContentValue),
+          },
+        };
+      }
 
-    const isAdded = !_.has(file1Content, key) && _.has(file2Content, key);
-    if (isAdded) {
-      return [...acc, { key, state: STATES.added, value: file2ContentValue }];
-    }
+      const isObjectValueDeleted = _.isPlainObject(file1ContentValue) && !_.has(currentLevelContent2, key);
+      if (isObjectValueDeleted) {
+        return {
+          ...acc,
+          [key]: {
+            state: STATES.deleted,
+            value: iter(file1ContentValue, file1ContentValue),
+          },
+        };
+      }
 
-    return [...acc, { key, state: STATES.modified, value: file2ContentValue, prevValue: file1ContentValue }];
-  }, []);
+      const isObjectValueAdded = !_.has(currentLevelContent1, key) && _.isPlainObject(file2ContentValue);
+      if (isObjectValueAdded) {
+        return {
+          ...acc,
+          [key]: {
+            state: STATES.added,
+            value: iter(file2ContentValue, file2ContentValue),
+          },
+        };
+      }
+
+      const isObjectValueModifiedToPrimitiveValue = _.isPlainObject(file1ContentValue);
+      if (isObjectValueModifiedToPrimitiveValue) {
+        return {
+          ...acc,
+          [key]: {
+            state: STATES.modified,
+            value: file2ContentValue,
+            prevValue: iter(file1ContentValue, file1ContentValue),
+          },
+        };
+      }
+
+      const isPrimitiveValueModifiedToObjectValue = _.isPlainObject(file2ContentValue);
+      if (isPrimitiveValueModifiedToObjectValue) {
+        return {
+          ...acc,
+          [key]: {
+            state: STATES.modified,
+            value: iter(file1ContentValue, file2ContentValue),
+            prevValue: file1ContentValue,
+          },
+        };
+      }
+
+      const isPrimitiveValueDoesNotChanged = file1ContentValue === file2ContentValue;
+
+      if (isPrimitiveValueDoesNotChanged) {
+        return { ...acc, [key]: { state: STATES.initial, value: file1ContentValue } };
+      }
+
+      const isPrimitiveValueDeleted = _.has(currentLevelContent1, key) && !_.has(currentLevelContent2, key);
+      if (isPrimitiveValueDeleted) {
+        return { ...acc, [key]: { state: STATES.deleted, value: file1ContentValue } };
+      }
+
+      const isPrimitiveValueAdded = !_.has(currentLevelContent1, key) && _.has(currentLevelContent2, key);
+      if (isPrimitiveValueAdded) {
+        return { ...acc, [key]: { state: STATES.added, value: file2ContentValue } };
+      }
+
+      return {
+        ...acc,
+        [key]: {
+          state: STATES.modified,
+          value: file2ContentValue,
+          prevValue: file1ContentValue,
+        },
+      };
+    }, {});
+  };
+
+  const result = iter(content1, content2);
+
+  return result;
 };
 
 const makeReport = (diffTree) => {
-  const sorted = _.sortBy(diffTree, ['key']);
+  const iter = (diff, level = 1) => {
+    if (!diff) {
+      return [];
+    }
 
-  const space = '  ';
+    const sorted = _.sortBy(Object.keys(diff));
 
-  const parsed = sorted
-    .reduce((acc, { key, state, value, prevValue }) => {
-      switch (state) {
-        case STATES.added:
-          return [...acc, `${space}+ ${key}: ${value}`];
+    const space = '  '.repeat(level);
 
-        case STATES.deleted:
-          return [...acc, `${space}- ${key}: ${value}`];
+    const parsed = sorted.reduce((acc, key) => {
+      const { state, value, prevValue } = diff[key];
 
-        case STATES.modified:
-          return [...acc, `${space}- ${key}: ${prevValue}`, `${space}+ ${key}: ${value}`];
+      const renderObjectValue = (currentValue) => {
+        const result = [...iter(currentValue, level + 2).join('')].join('');
+        return result;
+      };
 
-        case STATES.initial:
-          return [...acc, `${space}  ${key}: ${value}`];
+      const keyValueSeparator = value === '' ? ':' : ': ';
+      const keyPrevValueSeparator = prevValue === '' ? ':' : ': ';
 
-        default:
-          throw new Error(`Unexpected state: ${state}`);
+      if (state === STATES.added) {
+        if (_.isPlainObject(value)) {
+          return [
+            ...acc,
+            [space, '+ ', key, ': ', '{\n'].join(''),
+            renderObjectValue(value),
+            [space, '  ', '}', '\n'].join(''),
+          ];
+        }
+
+        return [...acc, `${space}${'+ '}${key}${keyValueSeparator}${value}\n`];
       }
-    }, [])
-    .join('\n');
 
-  return ['{', parsed, '}'].join('\n');
+      if (state === STATES.deleted) {
+        if (_.isPlainObject(value)) {
+          const result = [
+            ...acc,
+            [space, '- ', key, ': ', '{\n'].join(''),
+            renderObjectValue(value),
+            [space, '  ', '}', '\n'].join(''),
+          ];
+
+          return result;
+        }
+
+        return [...acc, `${space}${'- '}${key}${keyValueSeparator}${value}\n`];
+      }
+
+      if (state === STATES.modified) {
+        if (_.isPlainObject(value) && _.isPlainObject(prevValue)) {
+          return [
+            ...acc,
+            [space, '  ', key, ': ', '{\n'].join(''),
+            renderObjectValue(value),
+            [space, '  ', '}', '\n'].join(''),
+          ];
+        }
+
+        const isPrimitiveValueModifiedToObjectValue = _.isPlainObject(value);
+
+        if (isPrimitiveValueModifiedToObjectValue) {
+          return [
+            ...acc,
+            [`${space}${'- '}${key}${keyPrevValueSeparator}${prevValue}\n`].join(''),
+            [space, '  ', key, ': ', '{\n'].join(''),
+            renderObjectValue(value),
+            [space, '  ', '}', '\n'].join(''),
+          ];
+        }
+
+        const isObjectValueModifiedToPrimitiveValue = _.isPlainObject(prevValue);
+        if (isObjectValueModifiedToPrimitiveValue) {
+          return [
+            ...acc,
+            [space, '- ', key, ': ', '{\n'].join(''),
+            renderObjectValue(prevValue),
+            [space, '  ', '}', '\n'].join(''),
+            [`${space}${'+ '}${key}${keyValueSeparator}${value}\n`].join(''),
+          ];
+        }
+
+        return [
+          ...acc,
+          [`${space}${'- '}${key}${keyPrevValueSeparator}${prevValue}\n`].join(''),
+          [`${space}${'+ '}${key}${keyValueSeparator}${value}\n`].join(''),
+        ];
+      }
+
+      if (state === STATES.initial) {
+        if (_.isPlainObject(value)) {
+          return [
+            ...acc,
+            [space, '  ', key, ': ', '{\n'].join(''),
+            renderObjectValue(value),
+            [space, '  ', '}', '\n'].join(''),
+          ];
+        }
+
+        return [...acc, `${space}${'  '}${key}${keyValueSeparator}${value}\n`];
+      }
+
+      throw new Error(`Unexpected state: ${state}`);
+    }, []);
+
+    return parsed;
+  };
+
+  return ['{\n', `${iter(diffTree).join('')}`, '}'].join('');
 };
 
 export default ({ filepath1, filepath2, format = 'json', readFile = defaultReadFile }) => {
   const parse = makeParser(format);
 
-  const file1Content = parse(readFile(filepath1));
-  const file2Content = parse(readFile(filepath2));
+  const parsedContent1 = parse(readFile(filepath1));
+  const parsedContent2 = parse(readFile(filepath2));
 
-  const diffTree = makeDiff(file1Content, file2Content);
+  const diff = makeDiff(parsedContent1, parsedContent2);
 
-  const result = makeReport(diffTree);
+  const result = makeReport(diff);
 
   return result;
 };
