29297237906518fa4ac1fd12273e2da179914975
Adel Khamatov <adelkhamatov@yandex.ru>
2021-04-10 14:19:43 +0300

    Feature: implement plain formatter

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,19 +1,40 @@
-import { test, expect } from '@jest/globals';
+import { test, expect, describe } from '@jest/globals';
 import gendiff from '../src/index.js';
 import readFile from '../src/utils/readFixtureFile.js';
 
-const result = readFile('result.txt').trim();
+describe('Формат вывода: stylish', () => {
+  const result = readFile('stylish-result.txt').trim();
 
-test('Сравнение JSON файлов', () => {
-  const filepath1 = '/json/file1.json';
-  const filepath2 = '/json/file2.json';
+  test('Сравнение JSON файлов', () => {
+    const filepath1 = '/json/file1.json';
+    const filepath2 = '/json/file2.json';
 
-  expect(gendiff({ filepath1, filepath2, readFile })).toBe(result);
+    expect(gendiff({ filepath1, filepath2, readFile })).toBe(result);
+  });
+
+  test('Сравнение YAML файлов', () => {
+    const filepath1 = '/yaml/file1.yml';
+    const filepath2 = '/yaml/file2.yml';
+
+    expect(gendiff({ filepath1, filepath2, readFile })).toBe(result);
+  });
 });
 
-test('Сравнение YAML файлов', () => {
-  const filepath1 = '/yaml/file1.yml';
-  const filepath2 = '/yaml/file2.yml';
+describe('Формат вывода: plain', () => {
+  const result = readFile('plain-result.txt').trim();
+  const format = 'plain';
 
-  expect(gendiff({ filepath1, filepath2, readFile })).toBe(result);
+  test('Сравнение JSON файлов', () => {
+    const filepath1 = '/json/file1.json';
+    const filepath2 = '/json/file2.json';
+
+    expect(gendiff({ filepath1, filepath2, readFile, format })).toBe(result);
+  });
+
+  test('Сравнение YAML файлов', () => {
+    const filepath1 = '/yaml/file1.yml';
+    const filepath2 = '/yaml/file2.yml';
+
+    expect(gendiff({ filepath1, filepath2, readFile, format })).toBe(result);
+  });
 });
--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,6 +1,71 @@
 import _ from 'lodash';
 import { STATES } from '../constants.js';
 
+const getAddedMessage = (key, value) => `Property ${key} was added with value: ${value}`;
+
+const getUpdatedMessage = (key, value, prevValue) =>
+  `Property '${key}' was updated. From ${prevValue} to ${value}`;
+
 export default (data) => {
-  const iter = () => {};
+  const iter = (currentLevelData, prevPath = []) => {
+    const result = Object.keys(currentLevelData)
+      .sort()
+      .flatMap((key) => {
+        const { value, prevValue, state } = _.get(currentLevelData, [key]);
+
+        const fullPathToValue = [...prevPath, key].join('.');
+
+        if (state === STATES.modified) {
+          if (_.isPlainObject(value)) {
+            return [iter(value, [...prevPath, key]).join('\n')];
+          }
+
+          const valueToPrintFormat = {
+            string: (pureValue) => `'${pureValue}'`,
+            object: (pureValue) => {
+              if (pureValue === null) {
+                return pureValue;
+              }
+
+              return '[complex value]';
+            },
+            number: (pureValue) => pureValue,
+            boolean: (pureValue) => pureValue,
+          };
+
+          return getUpdatedMessage(
+            fullPathToValue,
+            valueToPrintFormat[typeof value](value),
+            valueToPrintFormat[typeof prevValue](prevValue)
+          );
+        }
+
+        if (state === STATES.added) {
+          const isAddedStringValue = typeof value === 'string';
+
+          if (isAddedStringValue) {
+            return getAddedMessage(`'${fullPathToValue}'`, `'${value}'`);
+          }
+
+          const isAddedComplexValue = value instanceof Object;
+
+          if (isAddedComplexValue) {
+            return getAddedMessage(`'${fullPathToValue}'`, '[complex value]');
+          }
+
+          return getAddedMessage(`'${fullPathToValue}'`, value);
+        }
+
+        if (state === STATES.deleted) {
+          return `Property '${fullPathToValue}' was removed`;
+        }
+
+        return [];
+      });
+
+    return result;
+  };
+
+  const result = iter(data, []).join('\n');
+  return result;
 };
