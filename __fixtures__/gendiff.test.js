3f866dd2fb3d7062f8dff72a3f18d240ddcb9ce6
Adel Khamatov <adelkhamatov@yandex.ru>
2021-04-10 22:12:01 +0300

    Refactor: change fixtures path

--- __fixtures__/gendiff.test.js
+++ __fixtures__/gendiff.test.js
@@ -6,15 +6,15 @@ describe('Формат вывода: stylish', () => {
   const result = readFile('stylish-result.txt').trim();
 
   test('Сравнение JSON файлов', () => {
-    const filepath1 = '/json/file1.json';
-    const filepath2 = '/json/file2.json';
+    const filepath1 = '/file1.json';
+    const filepath2 = '/file2.json';
 
     expect(gendiff(filepath1, filepath2, 'stylish', readFile)).toBe(result);
   });
 
   test('Сравнение YAML файлов', () => {
-    const filepath1 = '/yaml/file1.yml';
-    const filepath2 = '/yaml/file2.yml';
+    const filepath1 = '/file1.yml';
+    const filepath2 = '/file2.yml';
 
     expect(gendiff(filepath1, filepath2, 'stylish', readFile)).toBe(result);
   });
@@ -25,15 +25,15 @@ describe('Формат вывода: plain', () => {
   const format = 'plain';
 
   test('Сравнение JSON файлов', () => {
-    const filepath1 = '/json/file1.json';
-    const filepath2 = '/json/file2.json';
+    const filepath1 = '/file1.json';
+    const filepath2 = '/file2.json';
 
     expect(gendiff(filepath1, filepath2, format, readFile)).toBe(result);
   });
 
   test('Сравнение YAML файлов', () => {
-    const filepath1 = '/yaml/file1.yml';
-    const filepath2 = '/yaml/file2.yml';
+    const filepath1 = '/file1.yml';
+    const filepath2 = '/file2.yml';
 
     expect(gendiff(filepath1, filepath2, format, readFile)).toBe(result);
   });
@@ -44,15 +44,15 @@ describe('Формат вывода: json', () => {
   const format = 'json';
 
   test('Сравнение JSON файлов', () => {
-    const filepath1 = '/json/file1.json';
-    const filepath2 = '/json/file2.json';
+    const filepath1 = '/file1.json';
+    const filepath2 = '/file2.json';
 
     expect(gendiff(filepath1, filepath2, format, readFile)).toBe(result);
   });
 
   test('Сравнение YAML файлов', () => {
-    const filepath1 = '/yaml/file1.yml';
-    const filepath2 = '/yaml/file2.yml';
+    const filepath1 = '/file1.yml';
+    const filepath2 = '/file2.yml';
 
     expect(gendiff(filepath1, filepath2, format, readFile)).toBe(result);
   });
